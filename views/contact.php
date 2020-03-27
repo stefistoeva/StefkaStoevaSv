0894c8deacf23934f0d0f04f163a4c18a3afb73b
Stefi Stoeva <44346648+stefistoeva@users.noreply.github.com>
2020-03-27 15:12:24 +0200

    edit html and css certificate

--- views/contact.php
+++ views/contact.php
@@ -1,13 +1,15 @@
 <?php
+var_dump($_POST);
+exit;
 $myEmail = "stefitstoeva@gmail.com"; //contact@stefistoeva.com
 $from = $_POST['email'];
-$firstName = $_POST['first_name'];
-$lastName = $_POST['last_name'];
+$company = $_POST['company'];
 $subject = $_POST['subject'];
 $message = $_POST['message']; 
 
-if(isset($_POST['submit']) && $from && $firstName && $lastName && $subject && $message) {
+if(isset($_POST['submit']) && $from && $company && $subject && $message) {
     $headers = "From:" . $from;
+    var_dump($headers);
     // Always set content-type when sending HTML email
     // $headers = "MIME-Version: 1.0" . "\r\n";
     // $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
@@ -16,26 +18,26 @@ if(isset($_POST['submit']) && $from && $firstName && $lastName && $subject && $m
     // $headers .= 'Cc: info@websapex.com' . "\r\n"; // If you want add cc
     // $headers .= 'Bcc: boss@websapex.com' . "\r\n"; // If you want add Bcc
 
-    mail($myEmail, $subject, $message, $headers);
+    // mail($myEmail, $subject, $message, $headers);
 
     // Please specify your Mail Server - Example: mail.example.com.
-    ini_set("SMTP","mail.example.com");
+    // ini_set("SMTP","mail.example.com");
 
     // Please specify an SMTP Number 25 and 8889 are valid SMTP Ports.
-    ini_set("smtp_port","25");
+    // ini_set("smtp_port","25");
 
     // Please specify the return address to use
-    ini_set('sendmail_from', 'example@YourDomain.com');
+    // ini_set('sendmail_from', 'example@YourDomain.com');
 
     // mail($to,$subject,$message,$headers)
-    if(mail($to,$subject,$message,$headers)){
-        echo "<script>
-                alert('Mail has been sent Successfully.');
-            </script>";
-    } else{
-        echo "<script>
-                alert('EMAIL FAILED');
-            </script>";
-    }
+    // if(mail($to,$subject,$message,$headers)){
+    //     echo "<script>
+    //             alert('Mail has been sent Successfully.');
+    //         </script>";
+    // } else{
+    //     echo "<script>
+    //             alert('EMAIL FAILED');
+    //         </script>";
+    // }
 }
 ?>
